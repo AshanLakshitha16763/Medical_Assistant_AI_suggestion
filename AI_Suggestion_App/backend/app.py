baffc755683e12a75e6d98d014ce814d4b3aa4bf
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-11-18 12:27:36 +0530

    Update app.py
    - removed difflib

--- AI_Suggestion_App/backend/app.py
+++ AI_Suggestion_App/backend/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-import difflib
 
 app = Flask(__name__)
 CORS(app)
