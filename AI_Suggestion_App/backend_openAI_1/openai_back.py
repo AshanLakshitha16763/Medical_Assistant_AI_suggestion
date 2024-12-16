f62cc91b05d7c38d12c334a9e89ed7c1afda9a5f
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-12-16 16:27:14 +0530

    Update openai_back.py
    + Fetching Data on OpenAI
    + Printing completion in terminal

--- AI_Suggestion_App/backend_openAI_1/openai_back.py
+++ AI_Suggestion_App/backend_openAI_1/openai_back.py
@@ -1,19 +1,44 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+
 import os
 
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
 # Set OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")  # Set this in your environment variables
+# openai.api_key = os.getenv("OPENAI_API_KEY")  # Set this in your environment variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Set this in your environment variables
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 def generate_ai_suggestions(input_text, num_suggestions=3):
     try:
-        # Call OpenAI's completion API
+        
+
+
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",  
+            messages=[
+                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user."},
+                {"role": "user", "content": input_text}
+            ],
+            max_tokens=20,  
+            temperature=0.2  
+        )
+        print(completion.choices[0].message)
+       # refined_input = response.choices[0].message.content  
+       # return refined_input
+
+        
+        # Call OpenAI's completion API (Previous)
+        """
         response = openai.Completion.create(
-            engine="gpt-4",  # Replace with the desired model, e.g., 'gpt-4' or 'text-davinci-003'
+            engine="gpt-3.5-turbo-instruct",  # Replace with the desired model, e.g., 'gpt-4' or 'text-davinci-003'
             prompt=input_text,
             max_tokens=50,
             n=num_suggestions,
@@ -21,6 +46,8 @@ def generate_ai_suggestions(input_text, num_suggestions=3):
             temperature=0.7
         )
         return [choice['text'].strip() for choice in response['choices']]
+        """
+
     except Exception as e:
         print(f"Error generating suggestions: {e}")
         return []
