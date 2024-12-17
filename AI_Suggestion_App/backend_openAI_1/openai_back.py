5dd765d9a3749c95ad6e3c9feeb4e0406640dc68
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-12-17 13:03:15 +0530

    Update openai_back.py
    + System prompt updated
    + max tokens adjusted
    + gives 3 suggestions
    + temperature adjusted
    + suggestions parameter enhanced with 'for'

--- AI_Suggestion_App/backend_openAI_1/openai_back.py
+++ AI_Suggestion_App/backend_openAI_1/openai_back.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 CORS(app)
 
 load_dotenv()
-# Set OpenAI API key
-# openai.api_key = os.getenv("OPENAI_API_KEY")  # Set this in your environment variables
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Set this in your environment variables
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
@@ -24,29 +23,16 @@ def generate_ai_suggestions(input_text, num_suggestions=3):
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",  
             messages=[
-                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user."},
+                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user. for example, if the user says 'How' you can generate the text like 'How are you?', 'How is your day going?','How old are you' etc."},
                 {"role": "user", "content": input_text}
             ],
-            max_tokens=20,  
-            temperature=0.2  
+            max_tokens=12,  
+            temperature=1.5,
+            n=num_suggestions  
         )
-        print(completion.choices[0].message)
-       # refined_input = response.choices[0].message.content  
-       # return refined_input
-
-        
-        # Call OpenAI's completion API (Previous)
-        """
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo-instruct",  # Replace with the desired model, e.g., 'gpt-4' or 'text-davinci-003'
-            prompt=input_text,
-            max_tokens=50,
-            n=num_suggestions,
-            stop=None,
-            temperature=0.7
-        )
-        return [choice['text'].strip() for choice in response['choices']]
-        """
+       
+        suggestions = [choice.message.content for choice in completion.choices]
+        return suggestions
 
     except Exception as e:
         print(f"Error generating suggestions: {e}")
