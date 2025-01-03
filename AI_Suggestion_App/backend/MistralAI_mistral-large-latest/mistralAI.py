bd4823089022c3bbaf22cbe16f7661d87d99c65d
Ashan Lakshitha <143149942+AshanLakshitha16763@users.noreply.github.com>
2025-01-03 13:22:23 +0530

    update code for mistral ai

--- AI_Suggestion_App/backend/MistralAI_mistral-large-latest/mistralAI.py
+++ AI_Suggestion_App/backend/MistralAI_mistral-large-latest/mistralAI.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from openai import OpenAI
 from dotenv import load_dotenv
+from mistralai import Mistral
 import os
 
 app = Flask(__name__)
@@ -9,26 +9,22 @@ CORS(app)
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
 
-client = OpenAI(
-    base_url="https://api.groq.com/openai/v1",
-    api_key=os.environ.get("GROQ_API_KEY")
-)
-
-def generate_ai_suggestions(input_text):
+def generate_ai_suggestions(input_text, num_suggestions=3):
     try:
         
-        completion = client.chat.completions.create(
-            model="llama3-groq-70b-8192-tool-use-preview",
-
+        completion = client.chat.complete(
+            #model="mistral-large-latest",  
+            model="pixtral-large-latest",
             messages=[
-                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user. for example, if the user says 'How' you can generate the text like 'How are you?', 'How is your day going?','How old are you' etc. Do not include any phrases like: 'I am sorry but I do not have the capability to perform this task for you, I am happy to help you with any other queries you may have.', 'Here are a few text options that start with','Note that the provided function is in Python' etc. Also do not use numbering for the suggestion Just give one suggestion for the text completion."},
-                
+                {"role": "system", "content": "you are a text-generation model. You get the words from the user and generate the text which starts with the words given by the user. for example, if the user says 'How' you can generate the text like 'How are you?', 'How is your day going?','How old are you' etc.Do not include any phrases like: 'I am sorry but I do not have the capability to perform this task for you, I am happy to help you with any other queries you may have.', 'Here are a few text options that start with','Here are a few possible continuations for your word' etc. just complete what user is giving to you. remember you are a text generation model so keep in mind not to act like chatbot. you are giving three suggestions for a single input so make sure your three completetions must be different from each other."},
                 {"role": "user", "content": input_text}
             ],
             max_tokens=12,  
-            temperature=1.5, 
+            temperature=1.5,
+            n=num_suggestions  
         )
        
         suggestions = [choice.message.content for choice in completion.choices]
@@ -60,4 +56,5 @@ def get_first_prompt():
     return jsonify({'prompt': "Type something here..."})
 
 if __name__ == '__main__':
+   # app.run(host="0.0.0.0", port=int("8080"),debug=True)
     app.run(debug=True)
